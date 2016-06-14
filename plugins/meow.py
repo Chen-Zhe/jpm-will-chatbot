e5d55713b1aba8924a824ea647ed1b8872a70801
pinguin <last.scythe@gmail.com>
2016-06-14 17:40:22 +0800

    Test upload

---
+++ plugins/meow.py
@@ -0,0 +1,11 @@
+from will.plugin import WillPlugin
+from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
+
+
+class MeowPlugin(WillPlugin):
+
+    @respond_to("^meow")
+    def meow(self, message):
+        self.reply(message, "meow meow meow!")
+ 
+
---
+++ plugins/test.py
@@ -0,0 +1,11 @@
+from will.plugin import WillPlugin
+from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
+
+
+class TestMePlugin(WillPlugin):
+
+    @respond_to("^testme")
+    def testme(self, message):
+        self.reply(message, message)
+ 
+
