dcfe407c10536b2fcc46ff5ac537664e17595b3e
chenzhe <chenzhe.943@gmail.com>
2016-06-15 18:18:07 +0800

    phishtank

--- plugins/phishtank.py
+++ plugins/phishtank.py
@@ -12,7 +12,19 @@ class PTPlugin(WillPlugin):
             url = "http://"+ url
 
         response = requests.post("http://checkurl.phishtank.com/checkurl/",
-                            data={"url": url, "format": "json",
-                                  "app_key": "a44346b8167abe4ab6e177f0afe21f0628c57facaba5699085a7a1400b3e2789"}).text
+                            data={"url":encoded, "format": "json",
+                                  "app_key": "a44346b8167abe4ab6e177f0afe21f0628c57facaba5699085a7a1400b3e2789"}
+                                 ).text
+
+        json_response = json.loads(response)
+
+        success=json_response["meta"]["success"]
+
+        reply="\nPhishtank Report\nURL/URI: "+url\
+              +"\nTimestamp"+str(json_response["meta"]["timestamp"])\
+              +"\nSuccess: "+str(success)
+
+        if success:
+
 
         self.reply(message, response)
