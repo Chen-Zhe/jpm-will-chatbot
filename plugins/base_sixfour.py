f7b8c1f828d2dc0a9d4ab7da6d93c2b7e909da0a
HarveyLeo <harveyleo.lhh@gmail.com>
2016-06-15 18:31:38 +0800

    remove base64

--- plugins/base_sixfour.py
+++ plugins/base_sixfour.py
@@ -1,6 +1,6 @@
 from will.plugin import WillPlugin
 from will.decorators import respond_to, periodic, hear, randomly, route, rendered_template, require_settings
-import base64
+
 
 
 class BaseSixFourPlugin(WillPlugin):
