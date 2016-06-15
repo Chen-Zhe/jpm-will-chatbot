7046684b760deaa43f506aa15dbb3bbe0930deb8
chenzhe <chenzhe.943@gmail.com>
2016-06-16 00:07:02 +0800

    new stuff

--- plugins/phishtank.py
+++ plugins/phishtank.py
@@ -3,6 +3,7 @@ from will.decorators import respond_to, periodic, hear, randomly, route, rendere
 import requests
 
 import json
+import base64
 
 class PTPlugin(WillPlugin):
 
@@ -12,7 +13,7 @@ class PTPlugin(WillPlugin):
             url = "http://"+ url
 
         response = requests.post("http://checkurl.phishtank.com/checkurl/",
-                            data={"url":encoded, "format": "json",
+                            data={"url":base64.b64encode(url.encode("utf-8")), "format": "json",
                                   "app_key": "a44346b8167abe4ab6e177f0afe21f0628c57facaba5699085a7a1400b3e2789"}
                                  ).text
 
@@ -25,6 +26,6 @@ class PTPlugin(WillPlugin):
               +"\nSuccess: "+str(success)
 
         if success:
-
+            reply+="\nConfirmed Phishing Site? "+str(json_response["results"]["in_database"])
 
         self.reply(message, response)
