4c9d1bd8d3fb7f097c9e2432a626cc164c70e5c9
chenzhe <chenzhe.943@gmail.com>
2016-06-21 22:36:27 +0800

    whois

--- plugins/whois.py
+++ plugins/whois.py
@@ -11,11 +11,11 @@ class whois(WillPlugin):
         #ip address
         if text.count(".")==3:
 
-            result_text = requests.get("https://who.is/whois-ip/ip-address/"+ip,headers= {"User-Agent": "Mozilla/5.0"}).text
+            result_text = requests.get("https://who.is/whois-ip/ip-address/"+text,headers= {"User-Agent": "Mozilla/5.0"}).text
             whois_text = re.match('.*<div class="col-md-12 queryResponseBodyKey"><pre>(.*)</pre></div>',result_text, re.DOTALL).group(1)
             return self.reply(message,whois_text)
         else:
-            result_text = requests.get("http://api.whoapi.com/?apikey=3fb4ea768efd677bfbed2d705bc6f47a&r=whois&domain=" + url,
+            result_text = requests.get("http://api.whoapi.com/?apikey=3fb4ea768efd677bfbed2d705bc6f47a&r=whois&domain=" + text,
                                        headers={"User-Agent": "Mozilla/5.0"}).text
             whois_dict = json.loads(result_text)
             reply="registered: "+ whois_dict["registered"] + "date created: "+whois_dict["date_created"]
